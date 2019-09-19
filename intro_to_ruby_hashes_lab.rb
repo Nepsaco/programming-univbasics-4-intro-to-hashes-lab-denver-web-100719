b43c4836330e2cde332f27c3353903759c5a6559
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-19 16:25:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,11 +3,14 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  hash = {
+    name: Tobie,
+    dog: Rocky
+  };
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  
 end
 
 def id_generator
